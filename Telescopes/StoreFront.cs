1a264877fbc31a86e9b3eb225402be258ff4807e
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-03-23 12:17:30 -0500

    Changed Orders->Order.cs & added main implementation to StoreFront

--- Telescopes/StoreFront.cs
+++ Telescopes/StoreFront.cs
@@ -1,114 +1,97 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("================================================================================================================================");
-Console.WriteLine("================================================================================================================================");
-Console.WriteLine(" **********           **                                                              ********    **                            ");
-Console.WriteLine("/////**///           /**                                       ******                **//////    /**                            ");
-Console.WriteLine("    /**      *****   /**   *****    ******   *****    ******  /**///**   *****      /**         ******  ******   ******   ***** ");
-Console.WriteLine("    /**     **///**  /**  **///**  **////   **///**  **////** /**  /**  **///**     /********* ///**/  **////** //**//*  **///**");
-Console.WriteLine("    /**    /*******  /** /******* //*****  /**  //  /**   /** /******  /*******     ////////**   /**  /**   /**  /** /  /*******");
-Console.WriteLine("    /**    /**////   /** /**////   /////** /**   ** /**   /** /**///   /**////             /**   /**  /**   /**  /**    /**//// ");
-Console.WriteLine("    /**    //******  *** //******  ******  //*****  //******  /**      //******      ********    //** //******  /***    //******");
-Console.WriteLine("    //      //////  ///   //////  //////    /////    //////   //        //////      ////////      //   //////   ///      ////// ");
-Console.WriteLine("================================================================================================================================");
-Console.WriteLine("================================================================================================================================");
-Console.WriteLine("\nWelcome to Telescope Store!\n");
-
-string Username()
+public class StoreFront
 {
-EnterEUser:
-    Console.WriteLine("Username: ");
-    string username = Console.ReadLine() ?? "";
-
-    if (String.IsNullOrWhiteSpace(username))
+    public static void StoreTitle()
     {
-        Console.WriteLine("Enter a valid username: ");
-        goto EnterEUser;
-    }
-    return username;
-}
-
-string Password()
-{
-EnterEUser:
-    Console.WriteLine("Password: ");
-    string password = Console.ReadLine() ?? "";
-
-    if (String.IsNullOrWhiteSpace(password))
-    {
-        Console.WriteLine("Enter a valid password: ");
-        goto EnterEUser;
-    }
-    return password;
-}
-
-bool exit = false;
-
-while (!exit)
-{
-// ENTER COMMAND TO ENTER THE STORE MAIN PAGE
-EnterCommand:
-    Console.WriteLine("Enter a command: (S)ign up -- (L)og in -- (E)mployee -- (Q)uit");
-    string input = Console.ReadLine() ?? "";
-
-    if (String.IsNullOrWhiteSpace(input))
-    {
-        Console.WriteLine("Please enter a valid command: (S)ign up -- (L)og in -- (E)mployee -- (Q)uit");
-        goto EnterCommand;
+        Console.WriteLine("================================================================================================================================");
+        Console.WriteLine("================================================================================================================================");
+        Console.WriteLine(" **********           **                                                              ********    **                            ");
+        Console.WriteLine("/////**///           /**                                       ******                **//////    /**                            ");
+        Console.WriteLine("    /**      *****   /**   *****    ******   *****    ******  /**///**   *****      /**         ******  ******   ******   ***** ");
+        Console.WriteLine("    /**     **///**  /**  **///**  **////   **///**  **////** /**  /**  **///**     /********* ///**/  **////** //**//*  **///**");
+        Console.WriteLine("    /**    /*******  /** /******* //*****  /**  //  /**   /** /******  /*******     ////////**   /**  /**   /**  /** /  /*******");
+        Console.WriteLine("    /**    /**////   /** /**////   /////** /**   ** /**   /** /**///   /**////             /**   /**  /**   /**  /**    /**//// ");
+        Console.WriteLine("    /**    //******  *** //******  ******  //*****  //******  /**      //******      ********    //** //******  /***    //******");
+        Console.WriteLine("    //      //////  ///   //////  //////    /////    //////   //        //////      ////////      //   //////   ///      ////// ");
+        Console.WriteLine("================================================================================================================================");
+        Console.WriteLine("================================================================================================================================");
+        Console.WriteLine("\nWelcome to Telescope Store!\n");
     }
 
-    char command = input.Trim().ToUpper()[0];
-
-    string username = "";
-    string password = "";
-
-    // START OF SWITCH TO READ COMMAND AND READ USERNAME/PASSWORD
-    switch (command)
+    public static string ValidString()
     {
-        // CASE FOR SIGNING UP
-        case ('S'):
-            Console.WriteLine("Sign Up!");
+    EnterString:
+        string valid = Console.ReadLine() ?? "";
 
-            // ENTER USERNAME
-            username = Username();
+        if (String.IsNullOrWhiteSpace(valid))
+        {
+            Console.WriteLine("Enter a valid input: ");
+            goto EnterString;
+        }
+        return valid;
+    }
+    public static void Main()
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            StoreTitle();
 
-            // ENTER PASSWORD
-            password = Password();
+        EnterCommand:
+            Console.WriteLine("Enter a command: (S)ign up -- (L)og in -- (E)mployee -- (Q)uit");
+            string input = Console.ReadLine() ?? "";
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Please enter a valid command: (S)ign up -- (L)og in -- (E)mployee -- (Q)uit");
+                goto EnterCommand;
+            }
+
+            char command = input.Trim().ToUpper()[0];
+
+            string username = "";
+            string password = "";
+
+            switch (command)
+            {
+                case ('S'):
+                    Console.WriteLine("Sign Up!");
+                    Console.WriteLine("Username: ");
+                    username = ValidString();
+                    Console.WriteLine("Password: ");
+                    password = ValidString();
+                    break;
+
+                case ('L'):
+                    Console.WriteLine("Log In!");
+                    Console.WriteLine("Username: ");
+                    username = ValidString();
+                    Console.WriteLine("Password: ");
+                    password = ValidString();
+                    break;
+
+                case ('E'):
+                    Console.WriteLine("Employee Log In!");
+                    Console.WriteLine("Username: ");
+                    username = ValidString();
+                    Console.WriteLine("Password: ");
+                    password = ValidString();
+                    break;
+
+                case ('Q'):
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Incorrect command!");
+                    break;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Welcome");
+
             break;
-
-        // CASE FOR LOG IN
-        case ('L'):
-            Console.WriteLine("Log In!");
-
-            // ENTER USERNAME
-            username = Username();
-
-            // ENTER PASSWORD
-            password = Password();
-            break;
-
-        // CASE FOR EMPLOYEE IN
-        case ('E'):
-            Console.WriteLine("Employee Log In!");
-
-            // ENTER USERNAME
-            username = Username();
-
-            // ENTER PASSWORD
-            password = Password();
-            break;
-
-        // CASE FOR QUITING
-        case ('Q'):
-            exit = true;
-            break;
-
-        // CASE FOR DEFAULT
-        default:
-            Console.WriteLine("Incorrect command!");
-            break;
-    } // END OF SWITCH STATEMENT
-
-    Console.WriteLine(username + password);
-    break;
+        }
+    }
 }
