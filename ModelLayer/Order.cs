cfea77912d319cf356b78e8979ea0573d0621ae8
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-03-28 17:15:36 -0500

    Added order details

--- ModelLayer/Order.cs
+++ ModelLayer/Order.cs
@@ -3,4 +3,6 @@ namespace ModelLayer;
 public class Order
 {
     public decimal OrderTotal { get; set; }
+    public int OrderNumber { get; set; }
+    public List<Product> Products { get; set; }
 }
