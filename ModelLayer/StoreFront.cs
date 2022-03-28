092c86cb807b5a49c2d46858bdbaf0e44bf7c523
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-03-28 17:15:57 -0500

    Added storefront basic functionality

--- ModelLayer/StoreFront.cs
+++ ModelLayer/StoreFront.cs
@@ -2,9 +2,32 @@
 
 public class StoreFront
 {
-    //access database for products
+    public StoreFront(Customer customer)
+    {
+        _customer = customer;
+    }
+    private Customer _customer;
+    public Customer User
+    {
+        get => _customer;
+        set
+        {
+            _customer = value;
+        }
+    }
+    public void Home()
+    {
+        Console.Clear();
+        Greeting();
+    }
 
-    //oder history
+    public void DisplayProducts()
+    {
+        //call business layer
+    }
 
-    //cart
+    public void Greeting()
+    {
+        Console.WriteLine($"Greetings {User.UserName}!");
+    }
 }
