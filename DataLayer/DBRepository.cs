1fc13fad70274e34b6ab7e8e61b302f7ff0890e0
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-03-30 21:39:01 -0500

    Implement add customer. Adds customer to database

--- DataLayer/DBRepository.cs
+++ DataLayer/DBRepository.cs
@@ -12,47 +12,25 @@ public class DBRepository : IRepository
 
     public void AddCustomer(Customer customer)
     {
-        throw new NotImplementedException();
-    }
-}
-
-// FOR REFERENCE ON HOW TO CONNECT TO DB AND READ INFORMATION
-/*
-    public List<Issue> GetAllIssues()
-    {
-        List<Issue> allQuestions = new List<Issue>();
-
         SqlConnection connection = new SqlConnection(_connectionString);
         connection.Open();
 
-        SqlCommand cmd = new SqlCommand("SELECT * FROM Issues", connection);
-        SqlDataReader reader = cmd.ExecuteReader();
+        SqlCommand cmd = new SqlCommand(@"INSERT INTO Customers(Username)
+            VALUES(" + customer.UserName + ")", connection);
 
-        //This returns true if there are more rows to read, if not false
-        //This also advances the datareader to the next row
-        while(reader.Read())
+        try
         {
-            int id = reader.GetInt32(0);
-            string title = reader.GetString(1);
-            DateTime dateCreated = reader.GetDateTime(2);
-            string content = reader.GetString(3);
-            Boolean isClosed = reader.GetBoolean(4);
-            int score = reader.GetInt32(5);
-
-            Issue question = new Issue{
-                Id = id,
-                Title = title,
-                DateCreated = dateCreated,
-                Content = content,
-                IsClosed = isClosed,
-                Score = score
-            };
-            allQuestions.Add(question);
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+            cmd = new SqlCommand(@"CREATE TABLE Customer(
+                CustomerID INT PRIMARY KEY IDENTITY(1,1),
+                Username VARCHAR(30) NOT NULL UNIQUE,
+                IsEmployee BIT DEFAULT 0
+            )");
         }
 
-        reader.Close();
         connection.Close();
-
-        return allQuestions;
     }
-*/
+}
