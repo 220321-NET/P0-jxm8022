d655c8012ee3d6dd994cc4821e8e28b7986bf074
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-03 21:39:58 -0500

    Added interger and decimal validation

--- UILayer/InputValidation.cs
+++ UILayer/InputValidation.cs
@@ -18,4 +18,36 @@ public static class InputValidation
         }
         return valid;
     }
+
+    public static int ValidInteger()
+    {
+    EnterInteger:
+        int index;
+
+        if (Int32.TryParse(Console.ReadLine(), out index) && (index >= 0))
+        {
+            return index;
+        }
+        else
+        {
+            Console.WriteLine("Enter a valid input!");
+            goto EnterInteger;
+        }
+    }
+
+    public static decimal ValidDecimal()
+    {
+    EnterDecimal:
+        decimal price;
+
+        if (Decimal.TryParse(Console.ReadLine(), out price) && (price >= 0))
+        {
+            return price;
+        }
+        else
+        {
+            Console.WriteLine("Enter a valid input!");
+            goto EnterDecimal;
+        }
+    }
 }
