624b77cc5041afc1a736018d2159eba03a5ea550
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-07 13:06:30 -0500

    Added Customer tests

--- ModelLayer/Order.cs
+++ ModelLayer/Order.cs
@@ -3,7 +3,7 @@ namespace ModelLayer;
 public class Order
 {
     public decimal OrderTotal { get; set; }
-    public int TransactionID { get; set; }
+    public int TransactionID { get; set; } = -1;
     public string StoreName { get; set; } = "";
     public List<Product> Products { get; set; } = new List<Product>();
 
